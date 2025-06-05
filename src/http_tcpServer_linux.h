dfe2ac565d181d764585a0f7961488a6ba7bddd7
M1shaG <mg834247@gmail.com>
2025-06-05 15:59:14 +0300

    errorfix and assigning IP and port to socket

--- src/http_tcpServer_linux.h
+++ src/http_tcpServer_linux.h
@@ -1,16 +1,25 @@
 #ifndef INCLUDED_HTTP_TSCPSERVER_LINUX
 #define INCLUDED_HTTP_TSCPSERVER_LINUX
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 namespace http
 {
     class TcpServer
     {
         public:
-            TcpServer();
+            TcpServer(std::string, int port);
             ~TcpServer();
         private:
+            std::string m_ip_address;
+            int m_port;
+            int m_socket;
+            struct sockaddr_in m_socketAddress;
+            unsigned int m_socketAddress_len; 
 
 
+            int startServer();
+            int closeServer();
     };
 }
 
--- src/https_tcpServer_linux.cpp
+++ src/https_tcpServer_linux.cpp
@@ -1,4 +1,5 @@
 #include <http_tcpServer_linux.h>
+#include <iostream>
 
 namespace http
 {
@@ -7,22 +8,29 @@ namespace http
     {
         m_socket = socket(AF_INET, SOCK_STREAM, 0);
         if(m_socket < 0) {
-            cerr << "Cannot create socket"; 
+            std::cerr << "Cannot create socket"; 
             return 1;
         }
 
         return 0;   
+
+        if(bind(m_socket, (sockaddr *)&m_socketAddress, m_socketAddress_len) < 0) {
+            std::cerr << "Cannot connect socket to address";
+            return 1;
+        }
     }
 
-    void TcpServer::closeServer()
+    int TcpServer::closeServer()
     {
         close(m_socket);
         close(m_new_socket);
-        return 0;
+        return;
     }
 
-    TcpServer::TcpServer()
+    TcpServer::TcpServer(std::string ip_address, int port) : m_ip_address(ip_address), m_port(port), m_socketAddress_len(sizeof(m_socketAddress))
     {
+        m_socketAddress.sin_family = AF_INET;
+        m_socketAddress.sin_port = htons(m_port);
         startServer();
     }
 
